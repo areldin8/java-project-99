509d50f477b21aea5a710ac34019d29fdd766fa1
areldin8 <belphegor26ra@gmail.com>
2025-03-09 20:50:21 +0300

    task

--- app/src/main/java/hexlet/code/model/Task.java
+++ app/src/main/java/hexlet/code/model/Task.java
@@ -1,9 +1,19 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ManyToMany;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
